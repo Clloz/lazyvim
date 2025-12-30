fabbaec593f6c91c9b9b1331c8c9c02ddcedd937
Clloz <clloz@outlook.com>
2025-12-31 00:07:34 +0800

    chore: lazyvim config init

---
+++ README.md
@@ -0,0 +1,6 @@
+# 💤 LazyVim
+
+This is Clloz's LazyVim configuration.
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+if vim.g.vscode then
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+else
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+end

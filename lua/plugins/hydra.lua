4efd1dc6cd426d93151a07d7f445ec590fd909b7
Lei Zhang <zhanglei2@keenon.com>
2026-01-04 18:09:49 +0800

    feat: 修复resize mode的desc问题，新增nvim-scrollbar插件

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -1,13 +1,12 @@
 return {
   {
     "nvimtools/hydra.nvim",
-
-    -- 关键：只有在用到时才加载
     keys = {
-      "<leader>r",
-      desc = "Resize mode",
+      {
+        "<leader>r",
+        desc = "Resize mode",
+      },
     },
-
     config = function()
       local Hydra = require("hydra")
 
@@ -17,18 +16,11 @@ return {
         body = "<leader>r",
 
         heads = {
-          -- 左右
           { "h", "<cmd>vertical resize -2<cr>", { desc = "resize left" } },
           { "l", "<cmd>vertical resize +2<cr>", { desc = "resize right" } },
-
-          -- 上下
           { "j", "<cmd>resize +2<cr>", { desc = "resize down" } },
           { "k", "<cmd>resize -2<cr>", { desc = "resize up" } },
-
-          -- 重置
           { "=", "<cmd>wincmd =<cr>", { desc = "equalize windows" } },
-
-          -- 退出
           { "q", nil, { exit = true, desc = "quit resize mode" } },
           { "<Esc>", nil, { exit = true } },
         },

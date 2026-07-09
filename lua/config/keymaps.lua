1e5010b6696524d580e347d226615d50fc5d7101
Clloz <clloz@outlook.com>
2026-07-09 23:53:48 +0800

    chore: add yank path nvim keymap and oil keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,11 +11,53 @@ require("which-key").add({
 -- Move Lines
 map("n", "<leader>jj", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 map("n", "<leader>kk", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-map("i", "jj", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "kk", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+-- map("i", "jj", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+-- map("i", "kk", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 map("v", "<leader>jj", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 map("v", "<leader>kk", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 -- macro recording
 vim.keymap.set("n", "q", "<Nop>", { desc = "Disable macro recording on q" })
 vim.keymap.set("n", "Q", "q", { desc = "Record macro with Q" })
+
+-- Copy filename and path:line to clipboard
+local wk = require("which-key")
+
+wk.add({
+  { "<leader>y", group = "Yank path" },
+})
+
+local function copy(text, desc)
+  vim.fn.setreg("+", text)
+  vim.notify(desc)
+end
+
+vim.keymap.set("n", "<leader>yf", function()
+  copy(vim.fn.expand("%:t"), "Copied filename")
+end, {
+  desc = "Filename",
+})
+
+vim.keymap.set("n", "<leader>yp", function()
+  copy(vim.fn.expand("%"), "Copied relative path")
+end, {
+  desc = "Relative path",
+})
+
+vim.keymap.set("n", "<leader>yP", function()
+  copy(vim.fn.expand("%:p"), "Copied absolute path")
+end, {
+  desc = "Absolute path",
+})
+
+vim.keymap.set("n", "<leader>yr", function()
+  copy(("%s:%d"):format(vim.fn.expand("%"), vim.fn.line(".")), "Copied relative path:line")
+end, {
+  desc = "Relative path:line",
+})
+
+vim.keymap.set("n", "<leader>yR", function()
+  copy(("%s:%d"):format(vim.fn.expand("%:p"), vim.fn.line(".")), "Copied absolute path:line")
+end, {
+  desc = "Absolute path:line",
+})
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,4 +1,9 @@
 -- lua/plugins/oil.lua
+local function copy(value)
+  vim.fn.setreg("+", value)
+  vim.notify("Copied: " .. value)
+end
+
 return {
   "stevearc/oil.nvim",
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
@@ -8,6 +13,31 @@ return {
   opts = {
     view_options = {
       show_hidden = true,
+    },
+    keymaps = {
+      ["yf"] = function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if entry then
+          copy(entry.name)
+        end
+      end,
+      ["yp"] = function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if entry then
+          local path = vim.fs.joinpath(oil.get_current_dir(), entry.name)
+          copy(path)
+        end
+      end,
+      ["yP"] = function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if entry then
+          local path = vim.fs.baspath(vim.fs.joinpath(oil.get_current_dir(), entry.name))
+          copy(path)
+        end
+      end,
     },
   },
 }
